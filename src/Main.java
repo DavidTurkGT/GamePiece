762dfd33a4587cddcec370f9768bf52a821f79be
DavidTurkGT <dlturk747@gmail.com>
2017-07-18 16:48:06 -0400

    Fixed and added correct tests

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,66 @@
 public class Main {
     public static void main(String[] args) {
         GamePiece rook = new GamePiece();
-        rook.setColor("Black");
-        rook.setName("Rook");
-        rook.setGame("Chess-with-3-moves");
 
-        System.out.println("The " + rook.getName() + " is " + rook.getColor() );
-        System.out.println("The " + rook.getName() + " is playing " + rook.getGame() );
+        rook.setColor("Black");
+        if( rook.getColor() != "Black"){
+            System.out.println("The color is not black");
+            throw new AssertionError();
+        }
+
+        rook.setColor("White");
+        if(rook.getColor() != "White"){
+            System.out.println("The color is not white");
+            throw new AssertionError();
+        }
+
+        rook.setName("Rook");
+        if(rook.getName() != "Rook"){
+            System.out.println("The name is not rook");
+            throw new AssertionError();
+        }
+
+        rook.setName("Tower");
+        if(rook.getName() != "Tower"){
+            System.out.println("The name is not tower");
+            throw new AssertionError();
+        }
+
+        rook.setGame("Chess-with-3-moves");
+        if(rook.getGame() != "Chess-with-3-moves"){
+            System.out.println("Game is not Chess-with-3-moves");
+            throw new AssertionError();
+        }
+
+        rook.setGame("Real Chess");
+        if(rook.getGame() != "Real Chess"){
+            System.out.println("The game is not Real Chess");
+            throw new AssertionError();
+        }
 
         rook.move(2,15);
+        if(rook.getPositionX() != 2 || rook.getPositionY() != 15){
+            System.out.println("The rook is not at the right position (2, 15)");
+            throw new AssertionError();
+        }
         rook.freeze();
-        System.out.println("The " + rook.getName() + " is at the following position...");
-        System.out.println("Position x: " + rook.getPositionX() );
-        System.out.println("Position y: " + rook.getPositionY() );
-        if( rook.isFrozen() ){
-            System.out.println("The " + rook.getName() + " is frozen");
-        }
-
-        System.out.println("Moving again");
-        rook.move(1,8);
-        System.out.println("The " + rook.getName() + " is at the following position...");
-        System.out.println("Position x: " + rook.getPositionX() );
-        System.out.println("Position y: " + rook.getPositionY() );
-
-        System.out.println("Unfreezing");
-        rook.unfreeze();
         if( ! rook.isFrozen() ){
-            System.out.println("The " + rook.getName() + " is not frozen");
+            System.out.println("The rook is not frozen");
+            throw new AssertionError();
         }
+
+        rook.move(1,8);
+        if(rook.getPositionX() != 2 || rook.getPositionY() != 15){
+            System.out.println("The rook is not at the right position (2, 15)");
+            throw new AssertionError();
+        }
+
+        rook.unfreeze();
+        if(rook.isFrozen()){
+            System.out.println("The rook is not supposed to be frozen");
+            throw new AssertionError();
+        }
+
+        System.out.println("All tests pass!");
     }
 }
